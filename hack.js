a067e531899e6397e6b59f297e594e3569fbb3b4
GitJulle <105446170+GitJulle@users.noreply.github.com>
2023-09-29 10:37:19 +0200

    Add files via upload

---
+++ hack.js
@@ -0,0 +1,16 @@
+function passcrack(user) {
+  if (user == "mindSpace6504") {
+    alert("Password cracked! 6Carrot05")
+  }
+  else if (user == "mindiMoLethiana") {
+    alert("Password cracked! LeviathanIsComing")
+  }
+}
+
+function login(user, password) {
+  if (user == "mindSpace6504") {
+    if (password == "6Carrot05") {
+      window.open("https://pranx.com/matrix-code-rain/")
+    }
+  }
+}
